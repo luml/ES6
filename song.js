cc11bd795ed37802329893fce1e3c380d7bf3f02
Meiling Lu <1520622848@qq.com>
2019-02-26 15:12:33 +0800

    Javascript homework #1 Data Types
    It's about an album

---
+++ song.js
@@ -0,0 +1,31 @@
+/*
+ I will introduce one of my favourite Original Soundtracks
+*/
+
+const name = 'Grease'
+const year = '2015-09-18'
+const artists = 'Various Artists'
+const language = 'English'
+const school = 'Picture Soundtrack'
+const yearOfMovie = 1978
+// movie didn't release in China
+const releasedInChina = false
+// all songs in this original albumn
+var songs = ['Grease', 'Summer Nights', 'Hopelessly Devoted To You',
+            'You\'re The One That I What', 'Sandly',
+            'Beauty School Dropout', 'Look At Me I\'m Sandra Dee',
+            'Greased Lightning', 'It\'s Raining On Prom Night',
+            'Alone At A Drive In Movie','Blue Moon',
+            'Rock N\'Roll Is Here To Stay','Those Magic Changes',
+            'Hound Dog', 'Dorn To Hand Jive',
+            'Tears On My Pillow', 'Mooning', 'Freddy My Love',
+            'Rock N\'Roll Party Queen', 'There Are Worse Things I could Do',
+            'We Go Together', 'Love Is A Many Splendored Thing']
+
+// leading song in the movie
+var mainSong = 'You\'re The One That I What'
+
+console.log(name + " is a " + school + " from " + "Movie " + name + " in " + yearOfMovie)
+console.log("John Travolta is leading male role in " + artists)
+console.log("My favorite song from this soundtrack is " + "'" + songs[2] + "'")
+console.log("The cutiest song is " + "'" + songs[5] + "'")
